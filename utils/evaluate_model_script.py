586c0cfff3d9928a216014a8d36678b295de1acc
benfattori <ben.fattori@mail.utoronto.ca>
2021-04-27 08:31:59 -0400

    third commit

--- utils/evaluate_model_script.py
+++ utils/evaluate_model_script.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-from Stochastic_Depth_Nets.src.ResidualBlocks import StochasticDepthResNet
+from src.ResidualBlocks import StochasticDepthResNet
 from Train_Model import Train_Model
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     model = StochasticDepthResNet(
         filters_list=[16, 32, 64], N=18, p_L=0.5).cuda()
     model.load_state_dict(torch.load(
-        'Stochastic_Depth_Nets/last_weights.pth'))
+        'StochasticDepthResNets/models/ResNet110.pth'))
 
     model_class = Train_Model(model, None, test_data, None)
     t0 = time.time()
