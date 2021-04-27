761677eeb3e13384d020b153f5f51f74e14bb9ed
benfattori <ben.fattori@mail.utoronto.ca>
2021-04-27 18:08:27 -0400

    Cleaning up imports

--- utils/evaluate_model_script.py
+++ utils/evaluate_model_script.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-from StochasticDepthResNets.src.ResidualBlocks import StochasticDepthResNet, ResNet110
+from StochasticDepthResNets.src.ResidualBlocks import ResNet110
 from Train_Model import Train_Model
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
@@ -13,7 +13,6 @@ import time
 
 
 if __name__ == '__main__':
-    # Data augmentation as outlined in paper
     transform_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
