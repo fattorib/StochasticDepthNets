03b3418ac6f9f927a20b7c3a85f4352ecc1fa57b
benfattori <ben.fattori@mail.utoronto.ca>
2021-04-27 19:55:04 -0400

    Reformatting

--- train_model_script.py
+++ train_model_script.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from StochasticDepthResNets.src.ResidualBlocks import ResNet110
-from Train_Model import Train_Model
+from StochasticDepthResNets.utils.Train_Model import Train_Model
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from torchvision import transforms, utils
@@ -42,11 +42,8 @@ def main():
     train_data, val_data = torch.utils.data.random_split(train_data, [
         45000, 5000])
 
-    # # Training ResNet110
-    # model = ResNet110(pretrained=False)
-
-    # Training ResNet20
-    model = ResNet20(pretrained=False)
+    # Training ResNet110
+    model = ResNet110(pretrained=False)
 
     model_class = Train_Model(model, train_data, test_data, val_data)
     t0 = time.time()
